2094e7da9d7f469d9586473799d4f4ffcde3c558
ReeNoir <chernyshev1998@gmail.com>
2022-12-16 18:11:56 +0500

    Remake

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,4 @@
+/* Импорт модулей */ 
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
-
-import Swiper, { Navigation, Pagination } from 'swiper';
-
-const swiper = new Swiper();

0ffe1cb952c2aee9656ea52044074d3f6f3822ba
Aleksei Chernyshev <chernyshev1998@gmail.com>
2023-01-23 16:05:47 +0500

    fix upper

--- src/js/modules/upper.js
+++ src/js/modules/upper.js
@@ -1,16 +1,29 @@
 export function scrollUp(){
     const buttonUp = document.querySelector('#upper');
     const html = document.querySelector('html');
-    const mainHeight = document.querySelector('#main').offsetHeight
+    const main = document.querySelector('#main');
+    const footer = document.querySelector(`#footer`);
+    const projcets = document.querySelector(`#projects`);
     
     showButton();
 
     function showButton(){
-        if(scrollY > mainHeight && html.offsetWidth > 450) buttonUp.style.display = 'block'
+        if(scrollY > main.offsetHeight && html.offsetWidth > 450) buttonUp.style.display = 'block'
         else buttonUp.style.display = 'none'
     };
 
+    function moveUpButton(){
+        const scroll = window.innerHeight + scrollY;
+        const heightPage = main.offsetHeight + projcets.offsetHeight + footer.offsetHeight;
+        
+        if(scroll > heightPage){
+            buttonUp.style.bottom = `${scroll - heightPage}px`
+        }
+        else buttonUp.style.bottom = '45px'
+    }
+
     window.addEventListener('scroll', showButton);
+    window.addEventListener('scroll', moveUpButton);
 
     window.addEventListener('click', function(){
         html.scrollIntoView({
